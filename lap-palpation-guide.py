0bed391e9ea8f9654241b5d3c4bb7cc9c36128e9
Kerwin Caballas <kerwin.caballas@gmail.com>
2020-10-05 23:24:07 +0800

    Add lap-palpation-guide.py

--- lap-palpation-guide.py
+++ lap-palpation-guide.py
@@ -26,34 +26,28 @@ scale_y = int(bbox[3]) / lines
 
 x = np.linspace(start = int(bbox[0]), stop = int(bbox[0] + bbox[2]), num = lines)
 y = np.linspace(start = int(bbox[1]), stop = int(bbox[1] + bbox[3]), num = lines)
-# print(x)
-# print(y)
-
 
 while True:
     ok, frame = video.read()
     ok, bbox = tracker.update(frame)
     
-    if ok:
-        p1 = (int(bbox[0]), int(bbox[1]))
-        p2 = (int(bbox[0] + bbox[2]),
-              int(bbox[1] + bbox[3]))
-        
-        cv.rectangle(frame, p1, p2, (0, 0, 255), 2, 2)
-        
-        for line in np.arange(1, lines - 1):
-            
-#             line_p1 = (int(bbox[0] + int(bbox[0] + (scale_x * line))), int(bbox[1]))
-#             line_p2 = (int(bbox[0] + int(bbox[0] + (scale_x * line))), int(bbox[1] + bbox[3]))
+    p1 = (int(bbox[0]), int(bbox[1]))
+    p2 = (int(bbox[0] + bbox[2]),
+          int(bbox[1] + bbox[3]))
 
-            line_p1 = (int(x[line]), 
-                       int(bbox[1]))
-            line_p2 = (int(x[line]), 
-                       int(bbox[1] + bbox[3]))
-        
-            cv.line(frame, line_p1, line_p2, (0, 0, 255), 2, 2)
-        
-        cv.imshow('Tracking', frame)
-        k = cv.waitKey(1) & 0xff
-        if k == 27:
-            break
+    cv.rectangle(frame, p1, p2, (0, 0, 255), 2, 2)
+
+    for line in np.arange(0, lines):
+
+        vline_p1 = (int(bbox[0] + (scale_x * line)), int(bbox[1]))
+        vline_p2 = (int(bbox[0] + (scale_x * line)), int(bbox[1] + bbox[3]))
+        hline_p1 = (int(bbox[0]), int(bbox[1] + (scale_y * line)))
+        hline_p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + (scale_y * line)))
+
+        cv.line(frame, vline_p1, vline_p2, (0, 0, 255), 2, 2)
+        cv.line(frame, hline_p1, hline_p2, (0, 0, 255), 2, 2)
+
+    cv.imshow('Tracking', frame)
+    k = cv.waitKey(1) & 0xff
+    if k == 27:
+        break
